5ae4d963c71fc2b0ef61f065402b9a5dcfa0c2c3
Toshiki18 <68912243+Toshiki18@users.noreply.github.com>
2023-10-24 10:51:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 if uploaded_file is not None:
     df = pd.read_csv(
         uploaded_file,
-        sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+        #sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
         engine='python',
         na_values='-',
         header=None)
